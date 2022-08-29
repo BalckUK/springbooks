81d4aae28323f879d006c0c923b01c78650bd468
UMK <haga5032@naver.com>
2022-08-29 13:42:12 +0900

    JDBC 연결

--- ex00/src/main/java/org/zerock/sample/SampleHotel.java
+++ ex00/src/main/java/org/zerock/sample/SampleHotel.java
@@ -3,17 +3,21 @@ package org.zerock.sample;
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Component
 @ToString
 @Getter
+@RequiredArgsConstructor
 public class SampleHotel {
 	
+	@NonNull
 	private Chef chef;
 	
-	public SampleHotel(Chef chef) {
-		this.chef = chef;
-	}
+//	public SampleHotel(Chef chef) {
+//		this.chef = chef;
+//	}
 	
 }
